59580c3ba1b040a4c92bb4952450d950658c6c34
Brian <bk.wood@voafone.co.nz>
2021-05-08 20:56:13 +1200

    SSBO with packed nodes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use vulkano::buffer::cpu_pool::CpuBufferPool;
 use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
 use vulkano::command_buffer::{AutoCommandBufferBuilder, DynamicState, SubpassContents};
+use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::descriptor::PipelineLayoutAbstract;
 use vulkano::device::{Device, DeviceExtensions};
 use vulkano::framebuffer::{Framebuffer, FramebufferAbstract, RenderPassAbstract, Subpass};
 use vulkano::image::{ImageUsage, SwapchainImage};
-use vulkano::instance::{Instance, PhysicalDevice};
+use vulkano::instance::Instance;
+use vulkano::instance::PhysicalDevice;
 use vulkano::pipeline::viewport::Viewport;
 use vulkano::pipeline::GraphicsPipeline;
 use vulkano::swapchain;
@@ -15,19 +19,25 @@ use vulkano::sync;
 use vulkano::sync::{FlushError, GpuFuture};
 
 use vulkano_win::VkSurfaceBuild;
+use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
 
 use std::sync::Arc;
+use std::time::SystemTime;
 
 fn main() {
-    let required_extensions = vulkano_win::required_extensions();
-    let instance = Instance::new(None, &required_extensions, None).unwrap();
+    let instance = Instance::new(None, &vulkano_win::required_extensions(), None).unwrap();
     let physical = PhysicalDevice::enumerate(&instance).next().unwrap();
 
     let event_loop = EventLoop::new();
     let surface = WindowBuilder::new()
+        .with_title("Vulkano Test")
+        .with_inner_size(LogicalSize {
+            width: 720,
+            height: 720,
+        })
         .build_vk_surface(&event_loop, instance.clone())
         .unwrap();
 
@@ -38,6 +48,7 @@ fn main() {
 
     let device_ext = DeviceExtensions {
         khr_swapchain: true,
+        khr_storage_buffer_storage_class: true,
         ..DeviceExtensions::none()
     };
     let (device, mut queues) = Device::new(
@@ -78,6 +89,8 @@ fn main() {
         .unwrap()
     };
 
+    // Buffer Setup
+
     let vertex_buffer = {
         #[derive(Default, Debug, Clone)]
         struct Vertex {
@@ -87,13 +100,16 @@ fn main() {
 
         let vertex_array = [
             Vertex {
-                position: [-0.5, -0.25],
+                position: [1.0, 1.0],
             },
             Vertex {
-                position: [0.0, 0.5],
+                position: [-1.0, 1.0],
             },
             Vertex {
-                position: [0.25, -0.1],
+                position: [1.0, -1.0],
+            },
+            Vertex {
+                position: [-1.0, -1.0],
             },
         ]
         .iter()
@@ -102,24 +118,37 @@ fn main() {
         CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, vertex_array)
             .unwrap()
     };
-
+    
+    let _ = include_str!("shader.vert");
+    let _ = include_str!("shader.frag");
+    
     mod vs {
         vulkano_shaders::shader! {
             ty: "vertex",
             path: "src/shader.vert"
         }
     }
-
+    
     mod fs {
         vulkano_shaders::shader! {
             ty: "fragment",
             path: "src/shader.frag"
         }
     }
-
+    
     let vs = vs::Shader::load(device.clone()).unwrap();
     let fs = fs::Shader::load(device.clone()).unwrap();
+    
+    let uniform_buffer = CpuBufferPool::<fs::ty::Uniforms>::new(device.clone(), BufferUsage::all());
+    let data_buffer = {
+        // let data = (0..1000).map(|x| (x as f32 / 76.9864).sin().powi(2));
 
+        let root_node_bytes = [26u8, 201, 137, 0];
+        let root_node = u32::from_be_bytes(root_node_bytes);
+
+        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, std::iter::once(root_node)).unwrap()
+    };
+    
     let render_pass = Arc::new(
         vulkano::single_pass_renderpass!(
             device.clone(),
@@ -166,108 +195,139 @@ fn main() {
     let mut recreate_swapchain = false;
     let mut previous_frame_end = Some(sync::now(device.clone()).boxed());
 
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                *control_flow = ControlFlow::Exit;
-            }
-            Event::WindowEvent {
-                event: WindowEvent::Resized(_),
-                ..
-            } => {
-                recreate_swapchain = true;
-            }
-            Event::RedrawEventsCleared => {
-                previous_frame_end.as_mut().unwrap().cleanup_finished();
+    let time = SystemTime::now();
 
-                if recreate_swapchain {
-                    let dimensions: [u32; 2] = surface.window().inner_size().into();
-                    let (new_swapchain, new_images) =
-                        match swapchain.recreate_with_dimensions(dimensions) {
-                            Ok(r) => r,
-                            Err(SwapchainCreationError::UnsupportedDimensions) => return,
-                            Err(e) => panic!("Failed to recreate swapchain: {:?}", e),
-                        };
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        Event::WindowEvent {
+            event: WindowEvent::Resized(_),
+            ..
+        } => {
+            recreate_swapchain = true;
+        }
+        Event::RedrawEventsCleared => {
+            previous_frame_end.as_mut().unwrap().cleanup_finished();
 
-                    swapchain = new_swapchain;
-                    framebuffers = window_size_dependent_setup(
-                        &new_images,
-                        render_pass.clone(),
-                        &mut dynamic_state,
-                    );
-                    recreate_swapchain = false;
-                }
-
-                let (image_num, suboptimal, acquire_future) =
-                    match swapchain::acquire_next_image(swapchain.clone(), None) {
+            if recreate_swapchain {
+                let dimensions: [u32; 2] = surface.window().inner_size().into();
+                let (new_swapchain, new_images) =
+                    match swapchain.recreate_with_dimensions(dimensions) {
                         Ok(r) => r,
-                        Err(AcquireError::OutOfDate) => {
-                            recreate_swapchain = true;
-                            return;
-                        }
-                        Err(e) => panic!("Failed to acquire next image: {:?}", e),
+                        Err(SwapchainCreationError::UnsupportedDimensions) => return,
+                        Err(e) => panic!("Failed to recreate swapchain: {:?}", e),
                     };
 
-                if suboptimal {
-                    recreate_swapchain = true;
-                }
+                swapchain = new_swapchain;
+                framebuffers = window_size_dependent_setup(
+                    &new_images,
+                    render_pass.clone(),
+                    &mut dynamic_state,
+                );
+                recreate_swapchain = false;
+            }
 
-                let clear_values = vec![[0.3333, 0.6745, 0.1059, 1.0].into()];
-                let mut builder = AutoCommandBufferBuilder::primary_one_time_submit(
-                    device.clone(),
-                    queue.family(),
-                )
-                .unwrap();
+            // Update Buffers
 
-                builder
-                    .begin_render_pass(
-                        framebuffers[image_num].clone(),
-                        SubpassContents::Inline,
-                        clear_values,
-                    )
+            let dimensions = surface.window().inner_size();
+            let resolution = [dimensions.width as f32, dimensions.height as f32];
+            let time = time.elapsed().unwrap().as_millis() as f32;
+
+            let uniform_subbuffer = {
+                let uniform_data = fs::ty::Uniforms {
+                    resolution: resolution,
+                    time: time,
+                };
+
+                uniform_buffer.next(uniform_data).unwrap()
+            };
+
+            let uniform_layout = pipeline.descriptor_set_layout(0).unwrap();
+            let uniform_set = Arc::new(
+                PersistentDescriptorSet::start(uniform_layout.clone())
+                    .add_buffer(uniform_subbuffer)
                     .unwrap()
-                    .draw(
-                        pipeline.clone(),
-                        &dynamic_state,
-                        vertex_buffer.clone(),
-                        (),
-                        (),
-                        None,
-                    )
+                    .build()
+                    .unwrap(),
+            );
+            
+            let data_layout = pipeline.descriptor_set_layout(1).unwrap();
+            let data_set = Arc::new(
+                PersistentDescriptorSet::start(data_layout.clone())
+                    .add_buffer(data_buffer.clone())
                     .unwrap()
-                    .end_render_pass()
+                    .build()
+                    .unwrap(),
+            );
+
+            let (image_num, suboptimal, acquire_future) =
+                match swapchain::acquire_next_image(swapchain.clone(), None) {
+                    Ok(r) => r,
+                    Err(AcquireError::OutOfDate) => {
+                        recreate_swapchain = true;
+                        return;
+                    }
+                    Err(e) => panic!("Failed to acquire next image: {:?}", e),
+                };
+
+            if suboptimal {
+                recreate_swapchain = true;
+            }
+
+            let clear_values = vec![[0.3333, 0.6745, 0.1059, 1.0].into()];
+            let mut builder =
+                AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
                     .unwrap();
 
-                let command_buffer = builder.build().unwrap();
+            builder
+                .begin_render_pass(
+                    framebuffers[image_num].clone(),
+                    SubpassContents::Inline,
+                    clear_values,
+                )
+                .unwrap()
+                .draw(
+                    pipeline.clone(),
+                    &dynamic_state,
+                    vertex_buffer.clone(),
+                    (uniform_set.clone(), data_set.clone()),
+                    (),
+                    None,
+                )
+                .unwrap()
+                .end_render_pass()
+                .unwrap();
 
-                let future = previous_frame_end
-                    .take()
-                    .unwrap()
-                    .join(acquire_future)
-                    .then_execute(queue.clone(), command_buffer)
-                    .unwrap()
-                    .then_swapchain_present(queue.clone(), swapchain.clone(), image_num)
-                    .then_signal_fence_and_flush();
+            let command_buffer = builder.build().unwrap();
 
-                match future {
-                    Ok(future) => {
-                        previous_frame_end = Some(future.boxed());
-                    }
-                    Err(FlushError::OutOfDate) => {
-                        recreate_swapchain = true;
-                        previous_frame_end = Some(sync::now(device.clone()).boxed());
-                    }
-                    Err(e) => {
-                        println!("Failed to flush future: {:?}", e);
-                        previous_frame_end = Some(sync::now(device.clone()).boxed());
-                    }
+            let future = previous_frame_end
+                .take()
+                .unwrap()
+                .join(acquire_future)
+                .then_execute(queue.clone(), command_buffer)
+                .unwrap()
+                .then_swapchain_present(queue.clone(), swapchain.clone(), image_num)
+                .then_signal_fence_and_flush();
+
+            match future {
+                Ok(future) => {
+                    previous_frame_end = Some(future.boxed());
+                }
+                Err(FlushError::OutOfDate) => {
+                    recreate_swapchain = true;
+                    previous_frame_end = Some(sync::now(device.clone()).boxed());
+                }
+                Err(e) => {
+                    println!("Failed to flush future: {:?}", e);
+                    previous_frame_end = Some(sync::now(device.clone()).boxed());
                 }
             }
-            _ => (),
         }
+        _ => (),
     });
 }
 
--- src/shader.frag
+++ src/shader.frag
@@ -2,6 +2,130 @@
 
 layout(location = 0) out vec4 frag_colour;
 
+layout(set = 0, binding = 0) uniform Uniforms {
+    vec2 resolution;
+    float time;
+} u;
+
+layout(set = 1, binding = 0) buffer Data {
+    uvec4 data[];
+} d;
+
+bool RayBoxIntersect(vec3 rpos, vec3 rdir, vec3 vmin, vec3 vmax) {
+    vec3 bounds[2] = vec3[2](vmin, vmax);
+    vec3 invdir = 1.0 / rdir;
+    bool sign[3] = bool[3](invdir.x < 0, invdir.y < 0, invdir.z < 0);
+
+    float tmin, tmax, tymin, tymax, tzmin, tzmax;
+
+    tmin = (bounds[int(sign[0])].x - rpos.x) * invdir.x;
+    tmax = (bounds[1 - int(sign[0])].x - rpos.x) * invdir.x;
+    tymin = (bounds[int(sign[1])].y - rpos.y) * invdir.y;
+    tymax = (bounds[1 - int(sign[1])].y - rpos.y) * invdir.y;
+
+    if ((tmin > tymax) || (tymin > tmax))
+        return false;
+    if (tymin > tmin)
+        tmin = tymin;
+    if (tymax < tmax)
+        tmax = tymax;
+
+    tzmin = (bounds[int(sign[2])].z - rpos.z) * invdir.z;
+    tzmax = (bounds[1 - int(sign[2])].z - rpos.z) * invdir.z;
+
+    if ((tmin > tzmax) || (tzmin > tmax))
+        return false;
+    if (tzmin > tmin)
+        tmin = tzmin;
+    if (tzmax < tmax)
+        tmax = tzmax;
+
+    return true;
+}
+
+float RayBoxDist(vec3 rpos, vec3 rdir, vec3 vmin, vec3 vmax) {
+    float t[10];
+    t[1] = (vmin.x - rpos.x) / rdir.x;
+    t[2] = (vmax.x - rpos.x) / rdir.x;
+    t[3] = (vmin.y - rpos.y) / rdir.y;
+    t[4] = (vmax.y - rpos.y) / rdir.y;
+    t[5] = (vmin.z - rpos.z) / rdir.z;
+    t[6] = (vmax.z - rpos.z) / rdir.z;
+    t[7] = max(max(min(t[1], t[2]), min(t[3], t[4])), min(t[5], t[6]));
+    t[8] = min(min(max(t[1], t[2]), max(t[3], t[4])), max(t[5], t[6]));
+    t[9] = (t[8] < 0 || t[7] > t[8]) ? 0.0 : t[7];
+    return t[9];
+}
+
+vec3 RotateX(vec3 vec, float angle) {
+    return vec3(
+        vec.x, 
+        vec.y * cos(angle) - vec.z * sin(angle), 
+        vec.y * sin(angle) + vec.z * cos(angle)
+    );
+}
+
+vec3 RotateY(vec3 vec, float angle) {
+    return vec3(
+        vec.x * cos(angle) + vec.z * sin(angle), 
+        vec.y, 
+        -vec.x * sin(angle) + vec.z * cos(angle)
+    );
+}
+
+vec3 RotateZ(vec3 vec, float angle) {
+    return vec3(
+        vec.x * cos(angle) - vec.y * sin(angle), 
+        vec.x * sin(angle) + vec.y * cos(angle), 
+        vec.z
+    );
+}
+
+uint GetData(int index) {
+    int remainder = index % 4;
+    index /= 4;
+    return d.data[index][remainder];
+}
+
+uvec4 Unpack(uint i) {
+    uvec4 o;
+    o.x = i / 16777216;
+    o.y = (i - o.x * 16777216) / 65536;
+    o.z = (i - o.x * 16777216 - o.y * 65536) / 256;
+    o.w = i - o.x * 16777216 - o.y * 65536 - o.z * 256;
+    return o;
+}
+
 void main() {
-    frag_colour = vec4(0.098, 0.5686, 0.7529, 1.0);
+    vec2 st = gl_FragCoord.xy / u.resolution * vec2(1, -1) + vec2(0, 1);
+
+    int divisions = 1;
+    st = mod(st, 1.0 / float(divisions)) * float(divisions);
+
+    // float fov = 1.0;
+
+    // float p = u.time / 5000.0;
+
+    // vec3 rpos = vec3(sin(p) * 5, 3.5355, cos(p) * 5);
+    // vec3 rdir = vec3(fov * (st.x - 0.5), -1, fov * -(st.y - 0.5));
+    // rdir = RotateX(rdir, 0.9425);
+    // rdir = RotateY(rdir, p);
+
+    // float hit = RayBoxDist(rpos, rdir, vec3(-1, -1, -1), vec3(1, 1, 1));
+    // // bool hit = RayBoxIntersect(rpos, rdir, vec3(-1, -1, -1), vec3(1, 1, 1));
+    
+    // vec2 pos = vec2(0.5) - st;
+    // float r = length(pos) * 1.5;
+    // float a = ((atan(pos.y,pos.x) + 3.1415) / 3.1415 / 2.0) + u.time / 5000.0;
+
+    // vec3 col = vec3(smoothstep(4, 10, hit));
+    // // vec3 col = vec3(int(hit));
+    // // float d = data[int(st.y * 128.0)];
+    // // vec3 col = vec3(d);
+
+    uvec4 value = Unpack(GetData(0));
+
+    vec3 col = vec3(value.rgb / 256.0);
+
+    frag_colour = vec4(col, 0.0);
 }
